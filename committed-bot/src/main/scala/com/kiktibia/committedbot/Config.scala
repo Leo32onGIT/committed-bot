9d0e85c7f5bd91b0f039a438cec6711e9c9f4dfe
Leo32 <bwils120@hotmail.com>
2022-08-02 23:02:04 +1000

    Update Config.scala

--- committed-bot/src/main/scala/com/kiktibia/committedbot/Config.scala
+++ committed-bot/src/main/scala/com/kiktibia/committedbot/Config.scala
@@ -8,4 +8,5 @@ object Config {
   val token: String = root.getString("token")
   val guildId: String = root.getString("guild-id")
   val dataDir: String = root.getString("data-dir")
+	val circular = Iterator.continually(List(16711680, 16744192, 16776960, 65280, 255, 9699539)).flatten
 }
