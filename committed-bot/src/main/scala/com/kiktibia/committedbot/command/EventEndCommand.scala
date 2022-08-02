06c3af9fa4690ad18435525d1fc11f13848baeb1
Leo32 <bwils120@hotmail.com>
2022-08-03 07:58:37 +1000

    Update EventEndCommand.scala

--- committed-bot/src/main/scala/com/kiktibia/committedbot/command/EventEndCommand.scala
+++ committed-bot/src/main/scala/com/kiktibia/committedbot/command/EventEndCommand.scala
@@ -44,9 +44,11 @@ object EventEndCommand extends StrictLogging with Command {
     }
 
     embed.build()
-		val eventWriter = new BufferedWriter(new FileWriter("/home/data/committed-bot/event/0.dat", true))
-		eventWriter.write(s"")
-		eventWriter.close()
+
   }
 
+	val eventWriter = new BufferedWriter(new FileWriter("/home/data/committed-bot/event/0.dat", true))
+	eventWriter.write(s"")
+	eventWriter.close()
+	
 }
