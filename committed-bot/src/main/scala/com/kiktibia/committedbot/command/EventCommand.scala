1da6e9a37d57a93114d99befe8333fae79508334
Leo32 <bwils120@hotmail.com>
2022-08-02 21:27:46 +1000

    Update EventCommand.scala

--- committed-bot/src/main/scala/com/kiktibia/committedbot/command/EventCommand.scala
+++ committed-bot/src/main/scala/com/kiktibia/committedbot/command/EventCommand.scala
@@ -34,7 +34,7 @@ object EventCommand extends StrictLogging with Command {
     val embed = new EmbedBuilder()
 
 		// attempt to cycle through embed colors
-		val embedColorList = List(16711680, 16744192, 16776960, 65280, 255, 9699539)
+		val embedColorList = Set(16711680, 16744192, 16776960, 65280, 255, 9699539)
 
 		def random[T](s: Set[T]): T = {
 		  val n = util.Random.nextInt(s.size)
