ba01d7df6334dd2798ce457fa392ece518ea5a71
Leo32 <bwils120@hotmail.com>
2022-08-03 19:52:31 +1000

    Update BotApp.scala

--- committed-bot/src/main/scala/com/kiktibia/committedbot/BotApp.scala
+++ committed-bot/src/main/scala/com/kiktibia/committedbot/BotApp.scala
@@ -29,5 +29,5 @@ object BotApp extends App with StrictLogging {
   guild.updateCommands().addCommands(commands.asJava).complete()
 
 	private val adminCommands = EventEndCommand.command
-	guild.updatecommands().addCommands(adminCommands.asJava).setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_CHANNEL, Permission.MODERATE_MEMBERS)).complete()
+	guild.updateCommands().addCommands(adminCommands.asJava).setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_CHANNEL, Permission.MODERATE_MEMBERS)).complete()
 }
