b9d327ab8fcd42056ef64b2511f7c20e7a23d1c1
Leo32 <bwils120@hotmail.com>
2022-08-03 21:02:24 +1000

    Update EventEndCommand.scala

--- committed-bot/src/main/scala/com/kiktibia/committedbot/command/EventEndCommand.scala
+++ committed-bot/src/main/scala/com/kiktibia/committedbot/command/EventEndCommand.scala
@@ -11,7 +11,7 @@ import net.dv8tion.jda.api.interactions.commands.build.{Commands, SlashCommandDa
 object EventEndCommand extends StrictLogging with Command {
 
   val command: SlashCommandData = Commands.slash("eventend", "declare winners of event and start a new one")
-	  .setDefaultPermissions(DefaultMemberPermissions.DISABLED)
+	  .setDefaultPermissions(DISABLED)
 
   def handleEvent(): MessageEmbed = {
     logger.info("eventend command called")
