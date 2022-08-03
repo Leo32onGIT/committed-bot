674bdbad1bbcee727f33e39f9f868ec86c549d5e
Leo32 <bwils120@hotmail.com>
2022-08-03 21:15:48 +1000

    Update EventEndCommand.scala

--- committed-bot/src/main/scala/com/kiktibia/committedbot/command/EventEndCommand.scala
+++ committed-bot/src/main/scala/com/kiktibia/committedbot/command/EventEndCommand.scala
@@ -7,7 +7,7 @@ import com.typesafe.scalalogging.StrictLogging
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.entities.MessageEmbed
 import net.dv8tion.jda.api.interactions.commands.build.{Commands, SlashCommandData}
-import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions
+import net.dv8tion.jda.api.interactions.commands
 
 object EventEndCommand extends StrictLogging with Command {
 
